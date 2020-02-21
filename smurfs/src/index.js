3e9d318aeaf0d4ccb5edc2f6c452e5c7d313a46f
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-21 10:57:19 +0200

    create-steps-4-5-6

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "redux-thunk";
+import { creatStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "react-thunk";
+import { smurfReducer } from "./state/reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//STEP-4 USE combineReducers FROM REDUX TO MAKE A SINGLE REDUCER
+const combinedReducer = combineReducers({
+  smurf: smurfReducer
+});
+
+//STEP-5 USE creatStore FROM REDUX TO CREATE A STATE STORE
+const store = creatStore(
+  smurfReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      windows.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  // STEP-6 WRAP THE APPLICATION WITH A PROVIDER FROM REACT-REDUX
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
